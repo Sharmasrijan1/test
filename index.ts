7e48b4fe98098f6ba18205aa48aabfce13169c5a
Sharmasrijan1 <113118762+Sharmasrijan1@users.noreply.github.com>
2022-09-28 08:30:26 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { createServer } from "http";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { Server } from "socket.io";
+
+const app = express();
+const http = createServer(app);
+const path = require("path");
+
+const socketProxy = createProxyMiddleware("/socket", {
+  target: "wss://google.com",
+  changeOrigin: true,
+  ws: true,
+  logLevel: "debug",
+});
+
+app.use(socketProxy);
+app.use(express.static(path.join(__dirname, "../dist")));
+new Server(http);
+
+const PORT = process.env.PORT || 3001;
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
